1a7be09ebf8be2af2656806e7a3297f5b885d740
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-05-08 18:10:41 +0300

    feat: timer code

--- index.js
+++ index.js
@@ -2,23 +2,46 @@ const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
+let interval = null;
+
+function convertSeconds(time) {
+  const totalMin = Math.floor(time / 60);
+  const seconds = time % 60;
+  const hours = Math.floor(totalMin / 60);
+  const minutes = totalMin % 60;
+
+  let result = isZeroNeeded(hours) + ':' + isZeroNeeded(minutes) + ':' + isZeroNeeded(seconds);
+
+  return result;
+};
+
+function isZeroNeeded(time){
+  let result = `${time < 10 ? '0'+ time : time}`;
+  return result;
+}
+
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    interval = setInterval(() => {
+      timerEl.innerText = convertSeconds(seconds);
+      if (seconds <= 0) {
+        location.reload();
+      }
+      seconds--;
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
 inputEl.addEventListener('input', () => {
-  // Очистите input так, чтобы в значении
-  // оставались только числа
-});
+  inputEl.value = inputEl.value.replace(/\D/g,'');
+})
 
 buttonEl.addEventListener('click', () => {
   const seconds = Number(inputEl.value);
-
+  if (interval) clearInterval(interval)
+  buttonEl.innerText = "Stop";
   animateTimer(seconds);
-
   inputEl.value = '';
 });
